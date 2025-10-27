ef21118fb32e50ab358be0e7f5c7cd6748974b29
averagepojke <rw830685@gmail.com>
2025-10-27 20:44:18 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-export PATH=$PATH:/usr/local/bin:/usr/bin:/opt/bin
+which npm || echo "npm not found, installing Node.js..."
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
 npm install
 npm start
